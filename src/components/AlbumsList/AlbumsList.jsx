aa98781b2cb3eb71fb5c6fef347ef3fb30c56da9
Yash Dhasade <yash.dhasade@nimapinfotech.com>
2025-11-17 12:49:57 +0530

    second commit

--- src/components/AlbumsList/AlbumsList.jsx
+++ src/components/AlbumsList/AlbumsList.jsx
@@ -1,38 +1,47 @@
 import { useState } from 'react'
-import AlbumLogo from '../../assets/image.png'
+import Album from './Album/Album'
 import './albumsList.css'
 
 export default function AlbumsList(){
 
-    let [albums, setAlbums] = useState([{id: 1, name: 'Test Album' }])
+    const [albums, setAlbums] = useState([{id: 1, name: 'Test Album' },{id: 2, name: 'Test Album' },{id: 3, name: 'Test Album' },{id: 4, name: 'Test Album' },{id: 5, name: 'Test Album' },{id: 6, name: 'Test Album' },])
+    const [showForm , setShowForm] = useState(false)
+    const [formValue, setFormValue] =useState('')
+    const clearForm = ()=>{
+        setFormValue('')
+    }
+    const createAlbum = ()=>{
+        if(formValue.trim()){
+            setAlbums((prev)=>(
+                [{id: new Date().getTime(), name: formValue}, ...prev]
+            ))
+            clearForm()
+            setShowForm(false)
+        }
+    }
+
+
     return (
+
         <div className='album-list'>
+            {showForm && (
+            <div className = 'album-form'>
+                <div>
+                    <h1>Create an Album</h1>
+                </div>
+                <div className='input'>
+                    <input type='text' value={formValue} onChange={(e)=>setFormValue(e.target.value)} />
+                    <button className='red' onClick={clearForm}>Clear</button>
+                    <button className='blue' onClick={createAlbum}>Create</button>
+                </div>
+            </div>) 
+            }
+
             <div className='title'>
                 <h2>Your Albums</h2>
-                <button>Add album</button>
+                <button className={showForm ? 'cancel' :'add-album'} onClick={()=>setShowForm(!showForm)}>{showForm ? 'Cancel' :'Add album'}</button>
             </div>
-            <div className='albums'>
-                <div className='album'>
-                    <img className='album-img' src={AlbumLogo} alt='album image' />
-                    <h3>Test album</h3>
-                </div>
-                <div className='album'>
-                    <img className='album-img' src={AlbumLogo} alt='album image' />
-                    <h3>Test album</h3>
-                </div>
-                <div className='album'>
-                    <img className='album-img' src={AlbumLogo} alt='album image' />
-                    <h3>Test album</h3>
-                </div>
-                <div className='album'>
-                    <img className='album-img' src={AlbumLogo} alt='album image' />
-                    <h3>Test album</h3>
-                </div>
-                <div className='album'>
-                    <img className='album-img' src={AlbumLogo} alt='album image' />
-                    <h3>Test album</h3>
-                </div>
-            </div>
-        </div>
+            <Album albums={albums} />
+        </div>  
     )
 }
